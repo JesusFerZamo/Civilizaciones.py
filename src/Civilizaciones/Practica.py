2552582cb04372883dd435b76ff442784ca194eb
JesusFerZamo <jfzmora@gmail.com>
2018-02-19 13:41:16 +0100

    Diccionario

--- src/Civilizaciones/Practica.py
+++ src/Civilizaciones/Practica.py
@@ -19,6 +19,18 @@ c5 = input("Característica 5: ")
 r5 = input("Rango de " + c5)
 print("De acuerdo: ")
 
+diccionario = {"cadena": "abc", "numero": 42, "lista": [True, 42]}  # Diccionario que tiene diferentes valores por cada clave, incluso una lista
+print(diccionario["cadena"])  # Usando una clave, se accede a su valor
+print(diccionario["lista"][0])  # Acceder a un elemento de una lista dentro de un valor (del valor de la clave "lista", mostrar el primer elemento)
+diccionario["cadena"] = "xyz"  # Re-asignar el valor de una clave
+print(diccionario["cadena"])
+diccionario["decimal"] = 3.1415927  # Insertar un nuevo elemento clave:valor
+print(diccionario["decimal"])
+diccionario_mixto = {"tupla": (True, 3.1415), "diccionario": diccionario}  # También es posible que un valor sea un diccionario
+print(diccionario_mixto["diccionario"]["lista"][1])  # Acceder a un elemento dentro de una lista, que se encuentra dentro de un diccionario
+diccionario = {("abc",): 42}  # Sí es posible que una clave sea una tupla, pues es inmutable
+diccionario = {["abc"]: 42}  # No es posible que una clave sea una lista, pues es mutable, lo que provocará una excepción
+
 civi = {
     "Maya":0, "Azteca":0, "Incas":0, "Romana":0, "Egipcia":0
     }

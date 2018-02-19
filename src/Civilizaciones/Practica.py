15ba17db40b08e9592b0eff933cbfddc4da84eed
bal0n <franciscusxaverius@protonmail.com>
2018-02-19 13:33:03 +0100

    Actualización para obtención de rangos

--- src/Civilizaciones/Practica.py
+++ src/Civilizaciones/Practica.py
@@ -6,19 +6,18 @@ Created on 6 feb. 2018
 fichero = input("Nombre del fichero")
 fich = open(fichero+".txt", "w")
 
-print("Introduzca las 5 características")
+print("Introduzca las 5 características con sus rangos por favor")
 c1 = input("Característica 1: ")
+r1 = input("Rango de " + c1)
 c2 = input("Característica 2: ")
+r2 = input("Rango de " + c2)
 c3 = input("Característica 3: ")
+r3 = input("Rango de " + c3)
 c4 = input("Característica 4: ")
+r4 = input("Rango de " + c4)
 c5 = input("Característica 5: ")
+r5 = input("Rango de " + c5)
 print("De acuerdo: ")
-
-print(c1)
-print(c2)
-print(c3)
-print(c4)
-print(c5)
 
 civi = {
     "Maya":0, "Azteca":0, "Incas":0, "Romana":0, "Egipcia":0

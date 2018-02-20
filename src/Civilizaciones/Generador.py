297ff29a5e3f19f76592a5adac80c84ca1c1d29a
bal0n <franciscusxaverius@protonmail.com>
2018-02-20 18:01:21 +0100

    Optimización de Generador (sustituido listas por listas anidadas para mayor eficiencia)

--- src/Civilizaciones/Generador.py
+++ src/Civilizaciones/Generador.py
@@ -12,29 +12,25 @@ fich = open(fichero+".txt", "w")
 # Creación de listas
 c       = [] # Caracteristicas
 rmin    = [] # Rango mínimo
-rmax    = [] # Ranto máximo
-lista0  = []
-lista1  = []
-lista2  = []
-lista3  = []
-lista4  = []
+rmax    = [] # Ranto máximo    
+
+
+l = []
+for i in range(5) :
+    l.append([])
 
 # Almacenamiento de características y rangos por parte del usuario
 for x in range(5):
     c.append(input("Característica " + str(x) + ": "))
     rmin.append(int(input("Rango mínimo " + c[x] + ": ")))
     rmax.append(int(input("Rango máximo " + c[x] + ": ")))
-
-# Bucle para rellenar las diferentes listas siguiendo el rango establecido por el usuario
-for x in range(1000):
-    lista0.append(randrange(rmin[0],rmax[0]))
-    lista1.append(randrange(rmin[1],rmax[1]))
-    lista2.append(randrange(rmin[2],rmax[2]))
-    lista3.append(randrange(rmin[3],rmax[3]))
-    lista4.append(randrange(rmin[4],rmax[4]))
     
+    # Bucle para rellenar las diferentes listas siguiendo el rango establecido por el usuario
+    for i in range(1000):
+        l[x].append(randrange(rmin[x],rmax[x]))
+
 # Declaración de variable de tipo cadena salida para la extracción de los datos
-salida = c[0] + ":" + str(lista0).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[1] + ":" + str(lista1).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[2] + ":" + str(lista2).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[3] + ":" + str(lista3).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[4] + ":" + str(lista4).replace("[", "").replace("]", "").replace(" ", "")
+salida = c[0] + ":" + str(l[0]).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[1] + ":" + str(l[1]).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[2] + ":" + str(l[2]).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[3] + ":" + str(l[3]).replace("[", "").replace("]", "").replace(" ", "") + "\n" + c[4] + ":" + str(l[4]).replace("[", "").replace("]", "").replace(" ", "")
 
 print(salida) # Impresión por pantalla la salida
 fich.write(salida) # Escritura la salida en el fichero establecido

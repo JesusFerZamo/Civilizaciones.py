f94969da3e4d09fe6799389f4b3cab83268c5726
bal0n <franciscusxaverius@protonmail.com>
2018-02-24 21:56:03 +0100

    Añadida función para calcular la media de una lista

--- src/Civilizaciones/Extractor.py
+++ src/Civilizaciones/Extractor.py
@@ -6,6 +6,22 @@ Objetivo: media, moda, máximo, mínimo y varianza
 @author: javier
 '''
 
+'''
+FUNCIONES
+'''
+def media(l):
+    suma = 0
+    total = len(l)
+    
+    for i in range(total):
+        suma += int(l[i])
+    resultado = suma / total
+    return resultado
+
+
+'''
+CÓDIGO
+'''
 #n = input("Nombre del fichero (sin extensión): ")
 n = "ImperioRomano"
 f = open(n + ".txt", "r")
@@ -25,6 +41,6 @@ for i in range(len(l)):
 
 print(entrada)
 print(l[1].split(":")[1].split(","))
-
+print(media(l[1].split(":")[1].split(",")))
 f.close
 

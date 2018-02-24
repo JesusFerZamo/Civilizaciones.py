e4b6aa6c0fbd6056eec9af1795b1e844f37cd49e
bal0n <franciscusxaverius@protonmail.com>
2018-02-24 17:58:53 +0100

    Creada la matriz de elementos funcional

--- src/Civilizaciones/Extractor.py
+++ src/Civilizaciones/Extractor.py
@@ -12,7 +12,7 @@ f = open(n + ".txt", "r")
 l = [] # Lista primigenia
 n = [] # Lista de nombres de variables
 m = [] # Lista de elementos de cada variable
-k = []
+matrix = [] # Matriz de elementos numéricos
 n0 = []
 n1 = []
 n2 = []
@@ -26,10 +26,10 @@ l = entrada.split("\n")
 for i in range(len(l)):
     n.append(l[i].split(":")[0]) # Almacena los nombres de variables
     m.append(l[i].split(":")[1])
-    k.append([])
-    k[i] = m[i].split(",")
+    matrix.append([])
+    matrix[i] = m[i].split(",")
  
 print(entrada)
-print(k[2][0])
+print(matrix[2][0])
 
 f.close

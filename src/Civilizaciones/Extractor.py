7cd218da42d9154161c03c5b02608cda7fd27c35
bal0n <franciscusxaverius@protonmail.com>
2018-02-24 18:00:27 +0100

    Optimización de extractor

--- src/Civilizaciones/Extractor.py
+++ src/Civilizaciones/Extractor.py
@@ -13,11 +13,6 @@ l = [] # Lista primigenia
 n = [] # Lista de nombres de variables
 m = [] # Lista de elementos de cada variable
 matrix = [] # Matriz de elementos numéricos
-n0 = []
-n1 = []
-n2 = []
-n3 = []
-n4 = []
 
 entrada = f.read()
 
@@ -25,9 +20,9 @@ l = entrada.split("\n")
 
 for i in range(len(l)):
     n.append(l[i].split(":")[0]) # Almacena los nombres de variables
-    m.append(l[i].split(":")[1])
+    m.append(l[i].split(":")[1]) # Almacena listas de elementos
     matrix.append([])
-    matrix[i] = m[i].split(",")
+    matrix[i] = m[i].split(",")  # Almacena elementos de la lista de elementos en matrix
  
 print(entrada)
 print(matrix[2][0])
